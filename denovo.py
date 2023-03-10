b3b952fb4fe9ece56c9a13c5d3a18b142d33b64c
amisra7 <amisra7@devrmccoy1.rockfish.cluster>
2023-03-09 21:54:11 -0500

    add accommodations for all chrs

--- denovo.py
+++ denovo.py
@@ -1,18 +1,24 @@
 import sys 
+import time
+import os
 import numpy as np
+
+print("starting now!\n")
 
 # trios = map: child_id -> [father_id, mother_id]
 trios = np.genfromtxt("id_files/relations.txt", dtype=str, delimiter='\t', skip_header=1, autostrip=True)
 trios = {x[0]:x[1:] for x in trios}
 
+
+print("trios created!\n")
+
+
 # pop_info = map: child_id -> [population_code, superpopulation_code]
 pop_info = np.genfromtxt("igsr_samples.tsv", dtype=str, delimiter='\t', skip_header=1, usecols=(0,3,5), autostrip=True)
 pop_info = {r[0]:r[1:] for r in pop_info if r[0] in trios}
 
-# header = map: individual_id -> index in header
-# vcf data = genotype data from vcf
-vcf_data = np.genfromtxt("../chr21_small_phased_headerCat.vcf", dtype=str, delimiter='\t', comments="##", autostrip=True)
-header, vcf_data = {val: idx for idx, val in enumerate(vcf_data[0])}, vcf_data[1:]
+
+print("pop_info created!\n")
 
 
 def genotype_possible(row, child_allele, parent):
@@ -27,28 +33,56 @@ def genotype_possible(row, child_allele, parent):
 
     return True, parent_gt
 
-outfile = "variants.tsv"
 
+indir = "/home/amisra7/scratch16-rmccoy22/abortvi2/vcf_phasing_array/chm13_phased_vcf_header_fixed/"
+files = os.listdir(indir)
+outdir = "/home/amisra7/scratch4-rmccoy22/amisra7/denovo_tsvs/"
 
+for file in files:
+    chr = file.split('_')[0]
+    outfile = outdir + chr + '_variants.tsv'
+    
+    if os.path.isfile(outfile):
+        continue
+    
+    infile = indir + file
 
-with open(outfile, "w") as fp:
-    columns = ["#CHROM", "POS", "REF", "ALT", "CHILD_ID", "FATHER_ID", "MOTHER_ID", "CHILD_GT", "FATHER_GT", "MOTHER_GT", "POP", "SUPERPOP"]
-    fp.write('\t'.join(columns) + '\n')
+    print("starting data iteration for {0}!\n".format(chr))
 
-    for row in vcf_data:
+    with open(outfile, "w") as out_fp:
+        columns = ["#CHROM", "POS", "REF", "ALT", "CHILD_ID", "FATHER_ID", "MOTHER_ID", "CHILD_GT", "FATHER_GT", "MOTHER_GT", "POP", "SUPERPOP"]
+        out_fp.write('\t'.join(columns) + '\n')
 
-        for child, parents in trios.items():
-            child_gt = row[header[child]]
+        with open(infile, "r") as in_fp:
 
-            poss_pa, pa_gt = genotype_possible(row, child_gt[0], parents[0])
-            poss_ma, ma_gt = genotype_possible(row, child_gt[-1], parents[1])
+            while True:
+                row = in_fp.readline().strip()
 
-            if poss_pa and poss_ma:
-                continue
+                if not row:
+                    break
 
-            # line = row[0:2].tolist() + row[3:5].tolist() + [child] + parents + [child_gt, pa_gt, ma_gt] + pop_info[child]
-            line = row[0:2].tolist() + row[3:5].tolist() + [child] + parents.tolist() + [child_gt, pa_gt, ma_gt] + pop_info[child].tolist()
-            fp.write('\t'.join(line) + '\n')
-        
+                if row[0] == '#':
+                    if row[1] != "#":
+                        row = row.split('\t')
+                        header = {val: idx for idx, val in enumerate(row)}
+                    continue
 
+                row = row.split('\t')
+
+                for child, parents in trios.items():
+                    child_gt = row[header[child]]
+
+                    poss_pa, pa_gt = genotype_possible(row, child_gt[0], parents[0])
+                    poss_ma, ma_gt = genotype_possible(row, child_gt[-1], parents[1])
+
+                    if poss_pa and poss_ma:
+                        continue
+
+                    line = row[0:2] + row[3:5] + [child] + parents.tolist() + [child_gt, pa_gt, ma_gt] + pop_info[child].tolist()
+                    out_fp.write('\t'.join(line) + '\n')
+    
+    print("finished with data iteration for {0}!\n".format(chr))
+            
+
+print("finished all chromosomes variant identification!\n")
 
