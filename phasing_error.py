1c78ab18027eb9892a40497aa65e6ae3333535f1
amisra7 <amisra7@devrmccoy1.rockfish.cluster>
2023-03-11 17:51:29 -0500

    implement for all chrs

--- phasing_error.py
+++ phasing_error.py
@@ -3,16 +3,8 @@ import time
 import os
 import numpy as np
 
-#infile = "chr22_variants.tsv"
-infile = "chr22_variants_short.tsv"
-
-error_out = "var_phasing_errors.tsv"
-novel_out = "var_denovos.tsv"
-
-header = "\t".join(["#CHROM", "POS", "REF", "ALT", "CHILD_ID", "FATHER_ID", "MOTHER_ID", "CHILD_GT", "FATHER_GT", "MOTHER_GT", "POP", "SUPERPOP"])
-
-error_data = []
-novel_data = []
+# infile = "chr22_variants.tsv"
+# infile = "chr22_variants_short.tsv"
 
 def poss_phasing_err(child_gt, pa_gt, ma_gt):
     # homo = lambda gt : gt[0] == gt[-1]
@@ -34,30 +26,59 @@ def poss_phasing_err(child_gt, pa_gt, ma_gt):
 
     return False
 
+
+header = "\t".join(["#CHROM", "POS", "REF", "ALT", "CHILD_ID", "FATHER_ID", "MOTHER_ID", "CHILD_GT", "FATHER_GT", "MOTHER_GT", "POP", "SUPERPOP"])
+
+indir = "/home/amisra7/scratch4-rmccoy22/amisra7/denovo_tsvs/"
+files = os.listdir(indir)
+
+for file in files:
+    if len(file) < 4 or file[-4:] != ".tsv":
+        continue
     
+    print("beginning to split {0}!\n".format(indir + file))
+    chr = file.split('_')[0]
 
-with open(infile, "r") as fp:
-    while True:
-        line = fp.readline().strip()
+    outdir = indir + chr + '/'
 
-        if not line:
-            break
-        elif line[0] == '#':
-            continue
-        
-        data = line.split('\t')
+    os.mkdir(outdir)
 
-        # omit "trios" with only one parent
-        if data[5] == '0' or data[6] == '0':
-            error_data.append(line)
-        
-        elif poss_phasing_err(*data[7:10]):
-            error_data.append(line)
-        
-        else:
-            novel_data.append(line)
+    error_data = []
+    novel_data = []
+
+    error_out = chr + "_phasing_errors.tsv"
+    novel_out = chr + "_denovos.tsv"
+
+    infile = indir + file
+
+    with open(infile, "r") as fp:
+        while True:
+            line = fp.readline().strip()
+
+            if not line:
+                break
+            elif line[0] == '#':
+                continue
+            
+            data = line.split('\t')
+
+            # omit "trios" with only one parent
+            if data[5] == '0' or data[6] == '0':
+                error_data.append(line)
+            
+            elif poss_phasing_err(*data[7:10]):
+                error_data.append(line)
+            
+            else:
+                novel_data.append(line)
 
 
-np.savetxt(error_out, error_data, fmt='%s', newline='\n', header=header, comments='')
-np.savetxt(novel_out, novel_data, fmt='%s', newline='\n', header=header, comments='')
+    np.savetxt(outdir + error_out, error_data, fmt='%s', newline='\n', header=header, comments='')
+    np.savetxt(outdir + novel_out, novel_data, fmt='%s', newline='\n', header=header, comments='')
+    
+    os.rename(infile, outdir + file)
+    print("done splitting {0}!\n".format(indir + file))
 
+
+
+print("done splitting all chromosomes!")
